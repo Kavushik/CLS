8eff3167ee2509aa920e3ea5bd831c90e3efe324
Kavushik <119117321+Kavushik@users.noreply.github.com>
2023-09-25 12:01:04 +0530

    Add files via upload

--- LCS.cpp
+++ LCS.cpp
@@ -1,1 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// Returns length of LCS for X[0..m-1],
+// Y[0..n-1]
+int lcs(char* X, char* Y, int m, int n,
+        vector<vector<int> >& dp)
+{
+    if (m == 0 || n == 0)
+        return 0;
+    if (X[m - 1] == Y[n - 1])
+        return dp[m][n] = 1 + lcs(X, Y, m - 1, n - 1, dp);
+
+    if (dp[m][n] != -1) {
+        return dp[m][n];
+    }
+    return dp[m][n] = max(lcs(X, Y, m, n - 1, dp),
+                        lcs(X, Y, m - 1, n, dp));
+}
+
+// Backtracking function to print LCS
+string printLCS(char* X, char* Y, int m, int n,
+                vector<vector<int> >& dp)
+{
+    string lcs = "";
+    int i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (X[i - 1] == Y[j - 1]) {
+            lcs = X[i - 1] + lcs;
+            i--;
+            j--;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1])
+            i--;
+        else
+            j--;
+    }
+    return lcs;
+}
+
+// Driver code
+int main()
+{
+    char X[] = "AGGTAB";
+    char Y[] = "GXTXAYB";
+
+    int m = strlen(X);
+    int n = strlen(Y);
+    vector<vector<int> > dp(m + 1, vector<int>(n + 1, -1));
+    cout << "Length of LCS is " << lcs(X, Y, m, n, dp) << endl;
+    cout << "Longest Common Subsequence is " << printLCS(X, Y, m, n, dp);
+
+    return 0;
+}
